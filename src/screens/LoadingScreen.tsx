e1abb9d7f09937c70bff57409b46e01143d865a4
paulo <pauloalvarez21@gmail.com>
2025-05-21 10:12:02 -0500

    feat: config initial

---
+++ src/screens/LoadingScreen.tsx
@@ -0,0 +1,18 @@
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+
+export const LoadingScreen = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#007AFF" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
